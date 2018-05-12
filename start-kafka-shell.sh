d34e8fac2cda27a1de9a8bd87bf367a500d97c26
Erdi Olmezogullari <erdi.olmez@gmail.com>
2018-05-11 21:27:51 -0700

    Added network name

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+
+NETWORK_NAME=`docker network ls -f name=kafka* -q`
+
+docker network ls
+echo $NETWORK_NAME
+
+docker run --network=$NETWORK_NAME --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
